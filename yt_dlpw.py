10b7e8314bd83b0a6310cd8e5db8d99a3b1e6cf5
Jan Juračka <jan.juracka@tescan.com>
2024-07-09 09:21:04 +0200

    make default destination relative to working directory

--- yt_dlpw.py
+++ yt_dlpw.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 
 ffmpeg = Path(__file__).parent/'external'/'ffmpeg.exe'
-dest_location = Path(__file__).parent/'yt_down'
+dest_location = Path('.')/'yt_down'
 dest_location.mkdir(exist_ok=True, parents=True)
 
 args = sys.argv[1:]
